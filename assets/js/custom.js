ffdc8c5f04f2bf09a8385a029ae3ad7a6a1858e2
Rajib Sarker <redhat0593@gmail.com>
2024-01-05 15:02:31 +0600

    about us completed

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -2,7 +2,7 @@ window.onscroll = function () { scrollFunction() };
 
 function scrollFunction() {
     let scroll_top = window.scrollY;
-    if (scroll_top > 80) {
+    if (scroll_top > 150) {
         document.getElementById("header").style.backgroundColor = '#00000080';
     } else {
         document.getElementById("header").style.backgroundColor = "transparent";
@@ -10,6 +10,26 @@ function scrollFunction() {
 }
 
 var swiper = new Swiper(".mySwiper", {
+    pagination: {
+        el: ".swiper-pagination",
+        type: "fraction",
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+
+var swiper = new Swiper(".teamMember", {
+    spaceBetween: 0,
+    breakpoints: {
+        420: {
+            slidesPerView: 2,
+        },
+        1200: {
+            slidesPerView: 3,
+        },
+    },
     pagination: {
         el: ".swiper-pagination",
         type: "fraction",
