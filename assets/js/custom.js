97d8433486ca248c02bd9bd33ba4b045fad580af
Rajib Sarker <redhat0593@gmail.com>
2024-01-08 02:42:55 +0600

    desk done

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -3,11 +3,46 @@ window.onscroll = function () { scrollFunction() };
 function scrollFunction() {
     let scroll_top = window.scrollY;
     if (scroll_top > 150) {
-        document.getElementById("header").style.backgroundColor = '#00000080';
+        document.getElementById("header").style.backgroundColor = '#000000';
     } else {
         document.getElementById("header").style.backgroundColor = "transparent";
     }
 }
+
+var swiper = new Swiper(".featureSlider", {
+    loop: true,
+    spaceBetween: 22,
+    breakpoints: {
+        375: {
+            slidesPerView: 2,
+        },
+        768: {
+            slidesPerView: 4,
+        },
+        1200: {
+            slidesPerView: 5,
+        },
+    },
+});
+
+var swiper = new Swiper(".amenities", {
+    autoplay: true,
+    breakpoints: {
+        375: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        991: {
+            slidesPerView: 3,
+        },
+        1200: {
+            slidesPerView: 4,
+        },
+    },
+});
+
 
 var swiper = new Swiper(".mySwiper", {
     pagination: {
